4bf0c6762a23e2de9930e17ff704a47cef688333
Rashad <rashad.suleymanov83@gmail.com>
2024-04-22 14:50:43 +0400

    Claen Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-
 import { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-
-  // Создаем ссылку с помощью хука useRef
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // Пример использования ссылки
   useEffect(() => {
-    // Устанавливаем фокус на элемент при монтировании компонента
-
     if (inputRef.current) {
       inputRef.current.focus();
     }
-  }, []); // Пустой массив зависимостей означает, что эффект будет за
+  }, []);
 
   return (
     <>
-      {/* Привязываем ссылку к элементу input */}
       <input ref={inputRef} type="text" />
-      <button onClick={() => inputRef.current && inputRef.current.focus()}>Установить фокус</button>
+      <button onClick={() => inputRef.current && inputRef.current.focus()}>
+        Установить фокус
+      </button>
     </>
   );
 }
